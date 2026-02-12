8f718995133ea86007462d1ddb62bc86d2032dfb
msrtna <mohalimor64@gmail.com>
2026-02-12 05:24:57 +0330

    add Database with migration and DbContext

---
+++ WinForms-CI-Demo/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WinForms_CI_Demo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=WinFormsCIDemo;Trusted_Connection=True;");
+        }
+    }
+}
